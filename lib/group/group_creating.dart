7b397d83487168b32b95766b8ff7120799505241
Lvvv <11437223@qq.com>
2020-10-18 22:55:28 +0800

    20201018 22:55

--- lib/group/group_creating.dart
+++ lib/group/group_creating.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ape/common/widget/app_bar_with_one_icon.dart';
 import 'package:ape/global/global_router.dart';
 
@@ -9,7 +11,6 @@ class GroupCreating extends StatefulWidget {
 }
 
 class _GroupCreatingState extends State<GroupCreating> {
-
   @override
   void initState() {
     super.initState();
@@ -22,21 +23,85 @@ class _GroupCreatingState extends State<GroupCreating> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBarWithOneIcon(
         backgroundColor: Colors.green,
         centerTitle: '创建新群',
         actionIcon: Icon(Icons.save),
         actionName: '保存',
-        onPressed: (){
+        onPressed: () {
 //          NavigatorUtils.push(context, GlobalRouter.groupCreating);
         },
       ),
-      body:Container(
-        child: Text('Hello create group!'),
+      body: Column(
+        children: <Widget>[
+          _itemPhoto(),
+        ],
       ),
     );
   }
 
+  Widget _itemPhoto() {
+    return GestureDetector(
+      onTap: () {
+        _selectPhoto(context);
+      },
+      child: Center(
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 8,
+          ),
+          alignment: Alignment.center,
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black12,
+          ),
+          child: Container(
+            padding: EdgeInsets.only(top: 40),
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: Text('选择照片'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _selectPhoto(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.photo_camera),
+                title: Text('相机'),
+                onTap: () async {
+                  var imageFile =
+                      await ImagePicker.pickImage(source: ImageSource.camera);
+                  Navigator.pop(context);
+                  //_saveImage(imageFile);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_library),
+                title: Text('相册'),
+                onTap: () async {
+                  var imageFile =
+                      await ImagePicker.pickImage(source: ImageSource.gallery);
+                  Navigator.pop(context);
+                  //_saveImage(imageFile);
+                },
+              ),
+            ],
+          );
+        });
+  }
 }
